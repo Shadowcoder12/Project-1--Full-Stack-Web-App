6953dcca6035ee809764cc69f97bc5893b2b0e7d
lfonzi62 <lfontenette@berkeley.edu>
2019-02-16 16:12:16 -0800

    added AJAX call to append category title to index.html

---
+++ public/scripts/app.js
@@ -0,0 +1,29 @@
+const handleResponse =  (json) =>  { 
+    // storing the json to a variable
+let categoryData = json;
+for (var i =0 ;  i < categoryData.length; i++) {
+let category= categoryData[i].category;
+let title = categoryData[i].title;
+// appending different categories to certain parts of the page
+if (category === "Music" ) {
+$('.Music').append(`<li> ${title}</li>`); }
+if (category === "Sports") {
+$('#Sports').append(`<li> ${title}</li>`); }
+};
+}
+
+const handleError = (xhr, status, errorThrown) => console.log('uh oh');
+
+
+
+
+
+$(document).ready(function(){
+
+    $.ajax({
+    method: 'GET',
+    url: '/categories',
+    success: handleResponse,
+    error: handleError
+});
+})
