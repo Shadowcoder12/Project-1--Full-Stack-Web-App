5547420c3f2229ca3718a372e0ad2e9d66292463
Rhea Roy <rroy.rhea@gmail.com>
2019-02-22 10:35:46 -0800

    edit function works but have to refresh

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,6 +1,7 @@
 var $commentsList;
 var commentArray = [];
 
+
 $(document).ready(function(){
 
   $commentsList = $('#commentTarget');
@@ -28,11 +29,10 @@ $(document).ready(function(){
     $.ajax({
       method: 'DELETE',
       url: 'api/comments/'+$(this).attr('data-id'),
-      success: deleteBookSuccess,
-      error: deleteBookError
+      success: deleteCommentSuccess,
+      error: deleteCommentError
     });
   });
-
 
 
   $commentsList.on('click', '.edit-book-button', function() {
@@ -56,21 +56,23 @@ $(document).ready(function(){
   })
 
 
-
-
-
-
 });
 
 function getCommentHtml(comment) {
   return `
         <div class = "commentP"
+
           <p>
             <b>${comment.text}</b> &nbsp
+            <span class="edit-input" style="display: none">
+            <input type="text" value="${comment.title}" />
+            <button class="edit-book-submit-button" data-id="${comment._id}">Save</button>
+          </span>
             by ${comment.author}
           </p>
           <button class="edit-book-button">Edit</button>
           <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${comment._id}>Delete</button>
+
 
           </div>`;
     
@@ -120,10 +122,10 @@ function newCommentSuccess(json) {
 }
 
 function newCommentError() {
-  console.log('newbook error!');
+  console.log('newComment error!');
 }
 
-function deleteBookSuccess(json) {
+function deleteCommentSuccess(json) {
   var comment = json;
   console.log(json);
   var commentId = comment._id;
@@ -138,6 +140,6 @@ function deleteBookSuccess(json) {
   render();
 }
 
-function deleteBookError() {
-  console.log('deletebook error!');
+function deleteCommentError() {
+  console.log('deleteComment error!');
 }
